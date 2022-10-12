5e15b1ea6f25a8913bdbe1cd840bebbaac5c2402
Rodrigo Lúcio <luciodigo@gmail.com>
2022-10-11 22:31:45 -0300

    Melhorias no exception handler

--- order/src/main/java/br/com/lucio/order/infra/exception/GlobalExceptionHandler.java
+++ order/src/main/java/br/com/lucio/order/infra/exception/GlobalExceptionHandler.java
@@ -1,10 +1,9 @@
 package br.com.lucio.order.infra.exception;
 
+import br.com.lucio.order.application.exception.ResourceNotFoundException;
 import br.com.lucio.order.application.exception.ServiceException;
 import br.com.lucio.order.infra.exception.dto.ErrorResponseDTO;
 import br.com.lucio.order.infra.exception.dto.ErrorResponseFieldDTO;
-import org.springframework.dao.DataIntegrityViolationException;
-import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -20,33 +19,33 @@ import javax.validation.ConstraintViolationException;
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler({ConstraintViolationException.class})
-    public ResponseEntity<?> handleEmptyResultDataAccessException(ConstraintViolationException ex, WebRequest request) {
+    public ResponseEntity<?> handleConstraintViolationException(ConstraintViolationException ex, WebRequest request) {
         ErrorResponseFieldDTO messagesInvalidFields = ErrorResponseFieldDTO.buildMessagesInvalidFields(ex.getConstraintViolations());
         return super.handleExceptionInternal(ex, messagesInvalidFields, new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
-    @ExceptionHandler({Exception.class})
     @Override
-    protected ResponseEntity<Object> handleExceptionInternal(Exception exception, @Nullable Object body,
-                                                             HttpHeaders headers, HttpStatus status, WebRequest request) {
+    public ResponseEntity<Object> handleExceptionInternal(Exception exception, @Nullable Object body,
+                                                          HttpHeaders headers, HttpStatus status, WebRequest request) {
         ErrorResponseDTO errorBody = ErrorResponseDTO.buildErrorResponse(exception, status, request);
         return super.handleExceptionInternal(exception, errorBody, headers, status, request);
     }
 
     @ExceptionHandler({ServiceException.class})
     public ResponseEntity<Object> handleServiceException(ServiceException exception) {
-        return buildErrorResponseAndHandleExceptionInternal(exception, HttpStatus.BAD_REQUEST);
+        return buildErrorResponseAndHandleException(exception, HttpStatus.BAD_REQUEST);
     }
 
-    @ExceptionHandler({DataIntegrityViolationException.class})
-    public ResponseEntity<Object> handleDataIntegrityViolationException(DataIntegrityViolationException exception) {
-        return buildErrorResponseAndHandleExceptionInternal(exception, HttpStatus.BAD_REQUEST);
+    @ExceptionHandler({ResourceNotFoundException.class})
+    public ResponseEntity<Object> handleResourceNotFoundException(ResourceNotFoundException exception) {
+        return buildErrorResponseAndHandleException(exception, HttpStatus.NOT_FOUND);
     }
 
-    @ExceptionHandler({EmptyResultDataAccessException.class})
-    public ResponseEntity<Object> handleEmptyResultDataAccessException(EmptyResultDataAccessException exception) {
-        return buildErrorResponseAndHandleExceptionInternal(exception, HttpStatus.NOT_FOUND);
+    @ExceptionHandler({Exception.class})
+    public ResponseEntity<Object> handleGeneralException(Exception exception) {
+        return buildErrorResponseAndHandleException(exception, HttpStatus.BAD_REQUEST);
     }
-    private ResponseEntity<Object> buildErrorResponseAndHandleExceptionInternal(Exception exception, HttpStatus status) {
+
+    private ResponseEntity<Object> buildErrorResponseAndHandleException(Exception exception, HttpStatus status) {
         ErrorResponseDTO errorBody = ErrorResponseDTO.buildErrorResponse(exception, status);
         return super.handleExceptionInternal(exception, errorBody, new HttpHeaders(), status, null);
     }
