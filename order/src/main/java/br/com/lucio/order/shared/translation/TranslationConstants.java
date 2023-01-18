285e5ca048c12eb1d36caf392532beaf574be7fa
Rodrigo Lúcio <luciodigo@gmail.com>
2023-01-17 22:56:37 -0300

    Ajustado arquivo de constante das traduções

--- order/src/main/java/br/com/lucio/order/shared/translation/TranslationConstants.java
+++ order/src/main/java/br/com/lucio/order/shared/translation/TranslationConstants.java
@@ -1,8 +1,11 @@
 package br.com.lucio.order.shared.translation;
 
+import lombok.experimental.UtilityClass;
+
+@UtilityClass
 public class TranslationConstants {
 
-    public static String ORDER_NOT_FOUND_WITH_ID = "orders.order_not_found_with_id";
-    public static String ORDER_ITEM_NOT_FOUND_WITH_ID_FOR_ORDER_ID = "orders.order_item_not_found_with_id_for_order_id";
+    public static final String ORDER_NOT_FOUND_WITH_ID = "orders.order_not_found_with_id";
+    public static final String ORDER_ITEM_NOT_FOUND_WITH_ID_FOR_ORDER_ID = "orders.order_item_not_found_with_id_for_order_id";
 
 }
