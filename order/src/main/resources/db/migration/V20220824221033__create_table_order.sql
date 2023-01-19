23b04af146dd98fc625216def791d5536a09b3ec
Rodrigo Lúcio <luciodigo@gmail.com>
2023-01-18 22:24:47 -0300

    Ajustes SQL de migration

--- order/src/main/resources/db/migration/V20220824221033__create_table_order.sql
+++ order/src/main/resources/db/migration/V20220824221033__create_table_order.sql
@@ -1,7 +1,8 @@
 CREATE TABLE "order" (
   id uuid NOT NULL,
-  status varchar(255) NOT NULL,
-  created_at timestamp,
-  updated_at timestamp,
+  status VARCHAR(255) NOT NULL,
+  expected_delivery_date DATE,
+  created_at TIMESTAMP WITHOUT TIME ZONE,
+  updated_at TIMESTAMP WITHOUT TIME ZONE,
   CONSTRAINT pk_order_id PRIMARY KEY (id)
 );
--- order/src/main/resources/db/migration/V20220824221344__create_table_order_item.sql
+++ order/src/main/resources/db/migration/V20220824221344__create_table_order_item.sql
@@ -3,8 +3,8 @@ CREATE TABLE order_item (
   order_id uuid NOT NULL,
   description varchar(255) NOT NULL,
   quantity numeric(19) NOT NULL,
-  created_at timestamp,
-  updated_at timestamp,
+  created_at TIMESTAMP WITHOUT TIME ZONE,
+  updated_at TIMESTAMP WITHOUT TIME ZONE,
   CONSTRAINT pk_order_item_id PRIMARY KEY (id),
   CONSTRAINT fk_order_id FOREIGN KEY (order_id) REFERENCES "order"(id)
 );
