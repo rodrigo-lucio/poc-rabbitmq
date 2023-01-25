46ccc1ff2175e0c305c10ce08431523a5a8634ca
Rodrigo Lúcio <luciodigo@gmail.com>
2023-01-24 23:47:47 -0300

    Criado exchanges e listeners para pessoa criada, alterada e deletada

--- person/src/main/java/br/com/lucio/person/application/dto/PersonDTO.java
+++ person/src/main/java/br/com/lucio/person/application/dto/PersonDTO.java
@@ -3,10 +3,12 @@ package br.com.lucio.person.application.dto;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.ToString;
 
 import java.util.UUID;
 
 @Data
+@ToString
 public class PersonDTO {
 
     @JsonIgnore
--- person/src/main/java/br/com/lucio/person/infra/controller/PersonController.java
+++ person/src/main/java/br/com/lucio/person/infra/controller/PersonController.java
@@ -2,7 +2,10 @@ package br.com.lucio.person.infra.controller;
 
 import br.com.lucio.person.application.dto.PersonDTO;
 import br.com.lucio.person.application.service.PersonService;
+import br.com.lucio.person.infra.event.dto.PersonCreatedDTO;
+import br.com.lucio.person.infra.event.dto.PersonUpdatedDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -20,10 +23,14 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
+    @Autowired
+    private ApplicationEventPublisher publisher;
+
     @PostMapping
     public ResponseEntity<PersonDTO> create(@RequestBody @Valid PersonDTO dto, UriComponentsBuilder uriBuilder) {
         PersonDTO personCreated = personService.create(dto);
         URI uri = uriBuilder.path("/person/{id}").buildAndExpand(personCreated.getId()).toUri();
+        publisher.publishEvent(new PersonCreatedDTO(personCreated));
         return ResponseEntity.created(uri).body(personCreated);
     }
 
