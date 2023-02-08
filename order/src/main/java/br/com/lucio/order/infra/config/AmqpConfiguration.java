58d2fcd524b021b5214943fa4a885c1815ebf1f7
Rodrigo Lúcio <luciodigo@gmail.com>
2023-02-07 23:04:23 -0300

    Realizado o ack manual da mensagem e configurado um prefetch count de 500

--- order/src/main/java/br/com/lucio/order/infra/config/AmqpConfiguration.java
+++ order/src/main/java/br/com/lucio/order/infra/config/AmqpConfiguration.java
@@ -2,9 +2,12 @@ package br.com.lucio.order.infra.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.amqp.core.*;
+import org.springframework.amqp.rabbit.config.DirectRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.rabbit.listener.DirectMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.RabbitListenerContainerFactory;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
@@ -51,6 +54,17 @@ public class AmqpConfiguration {
         RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
         rabbitTemplate.setMessageConverter(messageConverter);
         return rabbitTemplate;
+    }
+
+    @Bean
+    public RabbitListenerContainerFactory<DirectMessageListenerContainer> rabbitListenerContainerFactory
+            (ConnectionFactory connectionFactory, Jackson2JsonMessageConverter messageConverter) {
+        DirectRabbitListenerContainerFactory factory = new DirectRabbitListenerContainerFactory();
+        factory.setConnectionFactory(connectionFactory);
+        factory.setAcknowledgeMode(AcknowledgeMode.MANUAL);
+        factory.setMessageConverter(messageConverter);
+        factory.setPrefetchCount(500);
+        return factory;
     }
 
 
--- order/src/main/java/br/com/lucio/order/infra/event/PersonReplicationEventListener.java
+++ order/src/main/java/br/com/lucio/order/infra/event/PersonReplicationEventListener.java
@@ -3,10 +3,15 @@ package br.com.lucio.order.infra.event;
 import br.com.lucio.order.application.service.PersonService;
 import br.com.lucio.order.infra.config.EventsConstants;
 import br.com.lucio.order.infra.event.dto.PersonCrudEventDTO;
+import com.rabbitmq.client.Channel;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
 
 @Component
 @Slf4j
@@ -15,10 +20,12 @@ public class PersonReplicationEventListener {
     @Autowired
     private PersonService service;
 
-    @RabbitListener(queues = EventsConstants.QUEUE_EVENTS_PERSON_CRUD)
-    public void personCreatedEvent(PersonCrudEventDTO personCrudEventDTO) {
+    @RabbitListener(queues = EventsConstants.QUEUE_EVENTS_PERSON_CRUD, containerFactory = "rabbitListenerContainerFactory")
+    public void personCreatedEvent(PersonCrudEventDTO personCrudEventDTO,
+                                   Channel channel, @Header(AmqpHeaders.DELIVERY_TAG) long tag) throws IOException {
         log.info("-----------> Received crud event for person: {}", personCrudEventDTO);
         service.crudEvent(personCrudEventDTO);
+        channel.basicAck(tag, false);
     }
 
 }
