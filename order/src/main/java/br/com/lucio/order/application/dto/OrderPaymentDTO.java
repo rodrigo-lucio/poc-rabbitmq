7c98929457deac8acbab9afda9d927979095f1b3
Rodrigo Lúcio <luciodigo@gmail.com>
2023-02-08 23:00:25 -0300

    Mapeado entidade e DTO orderPayment

---
+++ order/src/main/java/br/com/lucio/order/application/dto/OrderPaymentDTO.java
@@ -0,0 +1,40 @@
+package br.com.lucio.order.application.dto;
+
+import br.com.lucio.order.domain.entity.PaymentStatus;
+import br.com.lucio.order.domain.entity.Status;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Data
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class OrderPaymentDTO {
+
+    @JsonIgnore
+    @JsonProperty(value = "id")
+    private UUID id;
+
+    private String creditCardNumber;
+    private String cardHolderName;
+    private Integer validityMonth;
+    private Integer validityYear;
+    private Integer securityCode;
+    private BigDecimal amount;
+
+    @JsonIgnore
+    @JsonProperty(value = "status")
+    private PaymentStatus status;
+
+    @JsonIgnore
+    @JsonProperty(value = "dateConfirmed")
+    private LocalDateTime dateConfirmed;
+
+}
---
+++ order/src/main/java/br/com/lucio/order/domain/entity/OrderPayment.java
@@ -0,0 +1,87 @@
+package br.com.lucio.order.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.GenericGenerator;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "order_payment")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class OrderPayment {
+
+    @Id
+    @GeneratedValue(generator = "uuid2")
+    @GenericGenerator(name = "uuid2", strategy = "uuid2")
+    @Column(name = "id", updatable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", updatable = false)
+    private Order order;
+
+    @NotBlank
+    @Column(name = "credit_card_number")
+    @Size(max = 50)
+    private String creditCardNumber;
+
+    @NotBlank
+    @Column(name = "card_holder_name")
+    @Size(max = 255)
+    private String cardHolderName;
+
+    @NotNull
+    @Column(name = "validity_month")
+    @Size(max = 12)
+    private Integer validityMonth;
+
+    @NotNull
+    @Column(name = "validity_year")
+    @Size(max = 12)
+    private Integer validityYear;
+
+    @NotNull
+    @Column(name = "security_code")
+    @Size(max = 12)
+    private Integer securityCode;
+
+    @NotNull
+    @Column(name = "amount")
+    private BigDecimal amount;
+
+    @NotNull
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status")
+    private PaymentStatus status;
+
+    @Column(name = "date_confirmed", updatable = false)
+    private LocalDateTime dateConfirmed;
+
+    @CreatedDate
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+}
