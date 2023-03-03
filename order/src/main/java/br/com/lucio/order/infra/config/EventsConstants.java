fac1ce64ca8d454274c74c8fddae5461eb7dc1be
Rodrigo Lúcio <luciodigo@gmail.com>
2023-03-02 22:49:00 -0300

    Ajustes de constantes

--- order/src/main/java/br/com/lucio/order/infra/config/EventsConstants.java
+++ order/src/main/java/br/com/lucio/order/infra/config/EventsConstants.java
@@ -10,7 +10,8 @@ public class EventsConstants {
     public static final String QUEUE_EVENTS_PERSON_CRUD = "person.events.person-crud.order";
 
     //Publishers
-    public static final String EXCHANGE_EVENTS_PAYMENT_CREATED = "order.payment.events.payment-created";
+    public static final String EXCHANGE_EVENTS_PAYMENT_CREATED = "order.events.payment-created";
 
-
+    public static final String EXCHANGE_PAYMENT_EVENTS_PAYMENT_PROCESSED = "payment.events.payment-processed";
+    public static final String QUEUE_PAYMENT_EVENTS_PAYMENT_PROCESSED_ORDER = "payment.events.payment-processed.order";
 }
--- payment/src/main/java/br/com/lucio/payment/infra/config/EventsConstants.java
+++ payment/src/main/java/br/com/lucio/payment/infra/config/EventsConstants.java
@@ -5,10 +5,12 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class EventsConstants {
 
-    public static final String QUEUE_EVENTS_PAYMENT_CREATED = "order.payment.payment-created.payment";
-    public static final String EXCHANGE_EVENTS_PAYMENT_CREATED = "order.payment.events.payment-created";
+    public static final String QUEUE_EVENTS_PAYMENT_CREATED = "order.events.payment-created.payment";
+    public static final String EXCHANGE_EVENTS_PAYMENT_CREATED = "order.events.payment-created";
     public static final String EXCHANGE_PAYMENT_YOURSELF = "payment.yourself";
-    public static final String QUEUE_PAYMENT_PROCESSOR_YOURSELF = "payment.process-payment.yourself";
+    public static final String QUEUE_PAYMENT_PROCESS_PAYMENT_YOURSELF = "payment.process-payment.yourself";
+
+    public static final String QUEUE_PAYMENT_PROCESS_GATEWAY_YOURSELF = "payment.process-gateway.yourself";
     public static final String EXCHANGE_PAYMENT_EVENTS_PAYMENT_PROCESSED = "payment.events.payment-processed";
     public static final String QUEUE_PAYMENT_EVENTS_PAYMENT_PROCESSED_ORDER = "payment.events.payment-processed.order";
 }
