46bdbd2c8c7fd172c627c3d2512004d6ec8b19c5
Rodrigo Lúcio <luciodigo@gmail.com>
2023-03-08 22:46:36 -0300

    Update EventsConstants.java

--- payment/src/main/java/br/com/lucio/payment/infra/config/EventsConstants.java
+++ payment/src/main/java/br/com/lucio/payment/infra/config/EventsConstants.java
@@ -9,7 +9,6 @@ public class EventsConstants {
     public static final String EXCHANGE_EVENTS_PAYMENT_CREATED = "order.events.payment-created";
     public static final String EXCHANGE_PAYMENT_YOURSELF = "payment.yourself";
     public static final String QUEUE_PAYMENT_PROCESS_PAYMENT_YOURSELF = "payment.process-payment.yourself";
-
     public static final String QUEUE_PAYMENT_PROCESS_GATEWAY_YOURSELF = "payment.process-gateway.yourself";
     public static final String EXCHANGE_PAYMENT_EVENTS_PAYMENT_PROCESSED = "payment.events.payment-processed";
     public static final String QUEUE_PAYMENT_EVENTS_PAYMENT_PROCESSED_ORDER = "payment.events.payment-processed.order";
